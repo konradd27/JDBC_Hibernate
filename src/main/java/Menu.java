ca4094278f85286c63f633b39c5668fbeb9b8764
konradd27 <konradd27@gmail.com>
2023-01-09 17:54:38 +0100

    1st commit

---
+++ src/main/java/Menu.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Menu {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true){
+            System.out.println("Wybierz opcję: ");
+            System.out.println("1: Dodaj film");
+            System.out.println("2: Wyświetl film");
+            System.out.println("3: Zakończ program");
+
+            int input = scanner.nextInt();
+
+            switch(input){
+               case 1: System.out.println("Dodanie filmu");
+               break;
+
+               case 2:
+                    System.out.println("Wyświetlenie filmu");
+                    break;
+
+                case 3: break;
+
+                default:
+                    System.out.println("Błędny wybór");
+            }
+            if (input==3) break;
+
+        }
+    }
+}
