d54066e57d69f1d343573951531346ebd057107f
konradd27 <konradd27@gmail.com>
2023-01-09 18:39:42 +0100

    Fixed loop, Movie class added

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Menu {
-    public static void main(String[] args) {
+    public  void startMenu() {
 
         Scanner scanner = new Scanner(System.in);
 
---
+++ src/main/java/MoviesApp.java
@@ -0,0 +1,7 @@
+public class MoviesApp {
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+
+        menu.startMenu();
+    }
+}
